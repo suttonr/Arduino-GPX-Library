3d458973763d98b874f5921e38518fe89859e381
Ryan Sutton <ryan@ryanmsutton.com>
2011-02-27 23:34:27 -0500

    fix .h file

--- GPX/GPX.h
+++ GPX/GPX.h
@@ -1,6 +1,6 @@
 /*
- * GPX Library -- GPX.cpp
- * Created by: Ryan M Sutton
+ * GPX Library -- GPX.h
+ * Created by: Ryan Sutton
  *
  * Copyright (c) 2010, Ryan M Sutton
  * All rights reserved.
@@ -19,175 +19,88 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL Ryan M Sutton BE LIABLE FOR ANY
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
  */
 
-#include <GPX.h>
+#ifndef GPX_h
+#define GPX_h
 
-GPX::GPX(){
-}
-
-//Get methods
-
-String GPX::getOpen(){
-  return String(_GPX_HEAD);
-}
-//void printOpen(Print &ss){
-//  ss << F(_GPX_HEAD);
-//}
-
-String GPX::getClose(){
-  return String(_GPX_TAIL);
-}
-
-String GPX::getMetaData(){
-  String localStr(_GPX_META_HEAD);
-  if (_name.length() > 0){
-    localStr = localStr + String(_GPX_NAME_HEAD);
-    localStr = localStr + wrapCDATA(_name);
-    localStr = localStr + String(_GPX_NAME_TAIL);
-  }
-  if (_desc.length() > 0){
-    localStr = localStr + String(_GPX_DESC_HEAD);
-    localStr = localStr + wrapCDATA(_desc);
-    localStr = localStr + String(_GPX_DESC_TAIL);
-  }
-  localStr = localStr + String(_GPX_META_TAIL);
-  return localStr;
-
-}
-
-String GPX::getTrakOpen(){
-  return String(_GPX_TRAK_HEAD);
-}
-
-String GPX::getTrakClose(){
-  return String(_GPX_TRAK_TAIL);
-}
-
-String GPX::getTrakSegOpen(){
-  return String(_GPX_TRKSEG_HEAD);
-}
-
-String GPX::getTrakSegClose(){
-  return String(_GPX_TRKSEG_TAIL);
-}
-
-String GPX::getInfo(){
-  String localStr("");
-  if (_name.length() > 0){
-    localStr += _GPX_NAME_HEAD;
-    localStr += wrapCDATA(_name);
-    localStr += _GPX_NAME_TAIL;
-  }
-  if (_desc.length() > 0){
-    localStr += _GPX_DESC_HEAD;
-    localStr += wrapCDATA(_desc);
-    localStr += _GPX_DESC_TAIL;
-  }
-  return localStr;
-}
-
-int GPX::getPtOpen(char* s,long* lon, long* lat){
-  int latI = *lat/100000;
-  long latF = abs(*lat%100000);
-  int lonI = *lon/100000;
-  long lonF = abs(*lon%100000);
-  sprintf_P(s,PSTR(_GPX_PT_HEAD_FMT),latI,latF,lonI,lonF);
-  return 1;
-}
-
-String GPX::getPtOpen(String typ, long lon, long lat){
-  String localStr(_GPX_PT_HEAD);
-  localStr = localStr.replace("TYPE",typ);
-  localStr += formatPos(lat,100000) + "\" lon=\""; 
-  localStr += formatPos(lon,100000) + "\">";
-  //if (_ele != 999999999){
-  //if (_ele.length() > 0){
-  //  localStr += _GPX_ELE_HEAD;
-  //  localStr += _ele;
-  //  localStr += _GPX_ELE_TAIL;
-  //}
-  if (_sym.length() > 0){
-    localStr += _GPX_SYM_HEAD;
-    localStr += _sym;
-    localStr += _GPX_SYM_TAIL;
-  }
-  if (_src.length() > 0){
-    localStr += _GPX_SRC_HEAD;
-    localStr += wrapCDATA(_src);
-    localStr += _GPX_SRC_TAIL;
-  }
-  //localStr += String(_GPX_PT_TAIL).replace("TYPE",typ);
-  return localStr;
-}
-
-String GPX::getPtClose(String typ){
-  return String(_GPX_PT_TAIL).replace("TYPE",typ);;
-
-}
-
-String GPX::getStrParm(String parm,String ele){
-  String localStr("<");
-
-  localStr += parm;
-  localStr += ">";
-  localStr += ele;
-  localStr += "</";
-  localStr += parm;
-  localStr += ">";
-
-  return localStr;
-}
+#include <WProgram.h>
+#include <WString.h>
+#include <avr/pgmspace.h>
 
 
-String GPX::getLongParm(String parm,long ele){
-  String localStr("<");
-  
-  localStr += parm;
-  localStr += ">";
-  localStr += formatPos(ele,100);
-  localStr += "</";
-  localStr += parm;
-  localStr += ">";
-  
-  return localStr;
-}
-    
+#define _GPX_HEAD "<gpx version=\"1.1\" creator=\"Arduino GPX Lib\"\n xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n xmlns=\"http://www.topografix.com/GPX/1/1\"\n xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\"\n>\n"
+#define _GPX_TAIL               "</gpx>\n"
+#define _GPX_META_HEAD          "<metadata>"
+#define _GPX_META_TAIL          "</metadata>\n"
+#define _GPX_TRAK_HEAD          "<trk>"
+#define _GPX_TRAK_TAIL          "</trk>\n"
+#define _GPX_TRKSEG_HEAD        "<trkseg>"
+#define _GPX_TRKSEG_TAIL        "</trkseg>\n"
+#define _GPX_PT_HEAD            "<TYPE lat=\""
+#define _GPX_PT_TAIL            "</TYPE>\n"
+#define _GPX_PT_HEAD_FMT 	"<trkpt lat=\"%i.%0.5li\" lon=\"%i.%0.5li\">"
 
-//Set Methods
-void GPX::setName(String name){
-  _name = name;
-}
-void GPX::setDesc(String desc){
-  _desc = desc;
-}
-void GPX::setSym(String sym){
-  _sym = sym;
-}
-void GPX::setSrc(String src){
-  _src = src;
-}
+// Property Tags
+#define _GPX_NAME_HEAD          "<name>"
+#define _GPX_NAME_TAIL          "</name>\n"
+#define _GPX_DESC_HEAD          "<desc>"
+#define _GPX_DESC_TAIL          "</desc>\n"
+#define _GPX_SYM_HEAD           "<sym>"
+#define _GPX_SYM_TAIL           "</sym>\n"
+#define _GPX_ELE_HEAD           "<ele>"
+#define _GPX_ELE_TAIL           "</ele>\n"
+#define _GPX_SRC_HEAD           "<src>"
+#define _GPX_SRC_TAIL           "</src>\n"
+#define _GPX_TIME_HEAD          "<time>"
+#define _GPX_TIME_TAIL          "</time>\n"
 
-//Private Functions
-String GPX::wrapCDATA(String input){
-  String localStr("<![CDATA[");
-  localStr += input;
-  localStr += "]]>";
+// 'Public' Tags
+#define GPX_TRKPT               "trkpt"
+#define GPX_WPT                 "wpt"
+#define GPX_RTEPT               "rtept"
 
-  return localStr;
-}
+class GPX{
+  public:
+    GPX();
+    String getOpen();
+//    void printOpen(Print &stream);
+    String getClose();
+    String getMetaData();
+    String getTrakOpen();
+    String getTrakClose();
+    String getTrakSegOpen();
+    String getTrakSegClose();
+    String getInfo();
+    int getPtOpen(char* s,long* lon,long* lat);
+    String getPtOpen(String typ, long lon, long lat);
+    String getPtClose(String typ);
+    String getStrParm(String parm,String ele);
+    String getLongParm(String parm,long ele);
+    void setName(String name);
+    void setDesc(String desc);
+    void setSym(String sym);
+    void setSrc(String src);
+    void setTime(String time);
+  private:
+    //Variables
+    String _name;
+    String _desc;
+    String _sym;
+    String _src;
+    String _time;
 
-String GPX::formatPos(long input,long div){
-  String i(input/div);
-  i += ".";
-  i += abs(input%div);
-  return i; 
-}
+    //Functions
+    String wrapCDATA(String input);
+    String formatPos(long input,long div);
+};
+
+#endif
+
